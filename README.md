f81e413a818e0d6f52cd4392ff53ed1b2499fee7
Daniel Ibañez <danchrnez@gmail.com>
2018-07-20 15:54:01 +0800

    Added README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# IPA-MAP - Image Poly &lt;Area&gt; MAPper
+
+Split
+
+pang test lang para makita ung na drawing at kung kumagat ung hover sa image
+	
+<img src="https://raw.githubusercontent.com/theykneel/IPA-MAP/screenshots/split.png">
+
+Poly coordinates
+
+yung result ung ilalagay sa coords attribute at pang poly lang to hehehe
+	
+	<area shape="poly" coords="" href="#meow">
+
+<img src="https://raw.githubusercontent.com/theykneel/IPA-MAP/screenshots/select-coord-example.png">
+<img src="https://raw.githubusercontent.com/theykneel/IPA-MAP/screenshots/poly-coord-result.png">
+<img src="https://raw.githubusercontent.com/theykneel/IPA-MAP/screenshots/map-area-tag.png">
---
+++ ipa-map.js
@@ -0,0 +1,30 @@
+var output = false;
+var img = document.getElementById('image');
+var svg = document.getElementsByTagName('svg')[0];
+var x1, y1, x2, y2;
+
+svg.onclick = function(event){
+
+	if(event.target.classList.contains('svg')){
+		if(output == false){
+			document.getElementById('coords').innerHTML = event.layerX+','+event.layerY;
+			x1 = event.layerX;
+			y1 = event.layerY;
+			output = true;
+		}else{
+			document.getElementById('coords').innerHTML += ','+event.layerX+','+event.layerY;
+			x2 = event.layerX;
+			y2 = event.layerY;
+			document.getElementsByTagName('svg')[0].innerHTML += '<line x1="'+x1+'" y1="'+y1+'" x2="'+x2+'" y2="'+y2+'" />';
+			x1 = x2;
+			y1 = y2;
+		}
+	}
+	
+	
+}
+
+window.onload = function(){
+	document.getElementsByTagName('svg')[0].setAttribute("height", document.getElementById('image').height+"px");
+	document.getElementsByTagName('svg')[0].setAttribute("width", document.getElementById('image').width+"px");
+}
